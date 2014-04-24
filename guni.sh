0eeca962dbe0a9aa0e77f385dcdfa5da5e2137a8
Fredrik Nygård Carlsen <me@frecar.no>
2014-04-24 13:21:31 -0700

    create guni.sh

--- guni.sh
+++ guni.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -e
-LOGFILE=/opt/balder/guni.log
+LOGFILE=/home/frecar/webapps/fresno/guni.log
 LOGDIR=$(dirname $LOGFILE)
-NUM_WORKERS=8
+NUM_WORKERS=2
 USER=ubuntu
 GROUP=ubuntu
-ADDRESS=127.0.0.1:8010
-cd /opt/balder
-source /opt/balder/venv/bin/activate
+ADDRESS=127.0.0.1:8013
+cd /home/frecar/webapps/fresno/
+source /home/frecar/webapps/fresno/bin/activate
 test -d $LOGDIR || mkdir -p $LOGDI
 #export NEW_RELIC_CONFIG_FILE=newrelic.ini
-exec gunicorn balder.wsgi:application -w $NUM_WORKERS --bind=$ADDRESS \
-  --user=$USER --group=$GROUP --log-level=debug \
-  --log-file=$LOGFILE 2>>$LOGFILE
+exec gunicorn -w 4 -b 127.0.0.1:8012 fresno:app
